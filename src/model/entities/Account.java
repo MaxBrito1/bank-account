4d4505d4cd2534a37a0b4410db315011ac44289a
Max Brito <maxfelixsb@gmail.com>
2019-03-15 15:59:24 -0300

    Programa completo

--- src/model/entities/Account.java
+++ src/model/entities/Account.java
@@ -1,12 +1,14 @@
 package model.entities;
 
+import model.exceptions.DomainException;
+
 public class Account {
-	
+
 	private Integer number;
 	private String holder;
 	private Double balance;
 	private Double withdrawLimit;
-	
+
 	public Account() {
 	}
 
@@ -44,12 +46,19 @@ public class Account {
 	public void setWithdrawLimit(Double withdrawLimit) {
 		this.withdrawLimit = withdrawLimit;
 	}
-	
+
 	public void deposit(Double amount) {
 		balance += amount;
 	}
-	
+
 	public void withdraw(Double amount) {
+		if (amount > withdrawLimit) {
+			throw new DomainException("The amount exceeds withdraw limit");
+		}
+		if (amount > balance) {
+			throw new DomainException("Not enough balance");
+		}
+		
 		balance -= amount;
 	}
 
